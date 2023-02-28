df89c40b1884e3d0d5a97fc0f23baf4abc4a30c3
IrinaKolganova <irene1805@yandex.ru>
2023-03-01 00:24:44 +0300

    apdated

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ function sumAll(){let sum=+price11+ +price22+ +price33+ +price44;
     document.getElementById("resultSum").textContent = sum}
 sumAll();
 
+let resultFinal=document.getElementById("resultFinal");
 resultFinal.addEventListener('click', showResult, {once : true});
-function showResult() {let final = +document.getElementById("resultSum").value;
-let resultFinal=final*0.8;
-document.getElementById("resultFinal").textContent = resultFinal;  
+document.getElementById("resultSum").textContent = resultFinal;
+
+function showResult() {let final = resultFinal*0.8;
+//let resultFinal=final*0.8;
+  
 //final.addEventListener(`sumAll*0.8`, showResult);
 }
 //showResult();
